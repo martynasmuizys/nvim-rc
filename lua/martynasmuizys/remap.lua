1bb6415aed39e25c14f341d2e5a3f5d63beb32e5
Martynas Muizys <99730587+martynasmuizys@users.noreply.github.com>
2025-02-17 16:48:43 +0200

    freed from desire

--- lua/martynasmuizys/remap.lua
+++ lua/martynasmuizys/remap.lua
@@ -18,7 +18,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
