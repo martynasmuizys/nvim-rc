3d012ae104a143cac23ed9a9a1023125a3f32b00
Martynas Muizys <muizysmartynas@gmail.com>
2025-12-26 23:23:04 +0200

    spotify notis

--- lua/martynasmuizys/lazy/oil.lua
+++ lua/martynasmuizys/lazy/oil.lua
@@ -7,7 +7,11 @@ return {
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		-- Open parent directory in current window
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
