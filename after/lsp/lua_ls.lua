d8cf8108a57313582e11d4a9169b3e87675d1d69
Martynas-Muizys <Martynas.Muizys@ibm.com>
2026-04-14 14:43:08 +0300

    lua a boom

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -7,10 +7,10 @@ return {
 			diagnostics = {
 				globals = { "vim" },
 			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
+			-- workspace = {
+			-- 	library = vim.api.nvim_get_runtime_file("", true),
+			-- 	checkThirdParty = false,
+			-- },
 			format = {
 				enable = true,
 				defaultConfig = {
